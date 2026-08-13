67f815c7aa1a89402707d6d82462a247c2eae552
Hantz230 <dpf.cristian@gmail.com>
2026-08-13 08:49:24 -0300

    main atv2

--- Python&SQL/Atividade/BD&Phyton/relatorio.py
+++ Python&SQL/Atividade/BD&Phyton/relatorio.py
@@ -29,5 +29,21 @@ def relatorio_total():
         if conexao and conexao.is_connected():
             conexao.close()
 
+def produto_mais_caro():
+    conexao = None
+    try:
+        conexao = mysql.connector.connect(**DB_CONFIG)
+        cursor = conexao.cursor()
+        cursor.execute("select max(preco) from produtos;")
+        for p in cursor.fetchall():
+            print(f"{p[0]}")
+    except mysql.connector.Error as erro:
+        print(f"Erro ao listar: {erro}")
+    finally:
+        if conexao and conexao.is_connected():
+            conexao.close()
+
 relatorio()
 relatorio_total()
+produto_mais_caro()
+
